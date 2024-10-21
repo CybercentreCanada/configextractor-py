bdc82764e74f84a85de143df0755dc873f839e13
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-10-21 21:28:47 +0000

    Run tests with public projects based on Python compatibility

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -39,6 +39,7 @@ def test_mwcp_detection(cx):
             "https://github.com/jeFF0Falltrades/rat_king_parser",
             "rat_king_parser",
             ["rat_king_parser.extern.maco.rkp_maco.RKPMACO"],
+            10,
         ),
         (
             "https://github.com/apophis133/apophis-YARA-Rules",
@@ -48,21 +49,24 @@ def test_mwcp_detection(cx):
                 "apophis-YARA-Rules.scripts.maco_extractors.TrueBot_C2.TrueBot",
                 "apophis-YARA-Rules.scripts.maco_extractors.metastealer_decrypt_strings.MetaStealer",
             ],
+            8,
         ),
     ],
     ids=("jeFF0Falltrades/rat_king_parser", "apophis133/apophis-YARA-Rules"),
 )
-def test_public_projects(repository_url: str, extractor_path: str, extractors: list[str]):
+def test_public_projects(repository_url: str, extractor_path: str, extractors: list, python_minor: int):
     # Ensure that any changes we make doesn't break usage of public projects
     # which can affect downstream systems using like library (ie. Assemblyline)
     import os
+    import sys
 
     from git import Repo
     from tempfile import TemporaryDirectory
 
-    with TemporaryDirectory() as working_dir:
-        project_name = repository_url.rsplit("/", 1)[1]
-        Repo.clone_from(repository_url, os.path.join(working_dir, project_name))
+    if sys.version_info >= (3, python_minor):
+        with TemporaryDirectory() as working_dir:
+            project_name = repository_url.rsplit("/", 1)[1]
+            Repo.clone_from(repository_url, os.path.join(working_dir, project_name))
 
-        cx = ConfigExtractor([os.path.join(working_dir, extractor_path)], create_venv=True)
-        assert set(extractors) == set(cx.parsers.keys())
+            cx = ConfigExtractor([os.path.join(working_dir, extractor_path)], create_venv=True)
+            assert set(extractors) == set(cx.parsers.keys())
