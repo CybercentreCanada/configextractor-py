8c6790e5b3b3eb4c6e094130dd42cb0424a48fd2
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-10-21 20:00:39 +0000

    Add testing for public projects

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -30,3 +30,39 @@ def test_mwcp_detection(cx):
     # Ensure the subclass was detected
     assert "parsers.mwcp_extractor.TestMWCP" in cx.parsers
     assert "parsers.mwcp_extractor.Parser" not in cx.parsers
+
+
+@pytest.mark.parametrize(
+    "repository_url, extractor_path, extractors",
+    [
+        (
+            "https://github.com/jeFF0Falltrades/rat_king_parser",
+            "rat_king_parser",
+            ["rat_king_parser.extern.maco.rkp_maco.RKPMACO"],
+        ),
+        (
+            "https://github.com/apophis133/apophis-YARA-Rules",
+            "apophis-YARA-Rules",
+            [
+                "apophis-YARA-Rules.scripts.maco_extractors.Pikabot_V3_C2.Pikabot",
+                "apophis-YARA-Rules.scripts.maco_extractors.TrueBot_C2.TrueBot",
+                "apophis-YARA-Rules.scripts.maco_extractors.metastealer_decrypt_strings.MetaStealer",
+            ],
+        ),
+    ],
+    ids=("jeFF0Falltrades/rat_king_parser", "apophis133/apophis-YARA-Rules"),
+)
+def test_public_projects(repository_url: str, extractor_path: str, extractors: list[str]):
+    # Ensure that any changes we make doesn't break usage of public projects
+    # which can affect downstream systems using like library (ie. Assemblyline)
+    import os
+
+    from git import Repo
+    from tempfile import TemporaryDirectory
+
+    with TemporaryDirectory() as working_dir:
+        project_name = repository_url.rsplit("/", 1)[1]
+        Repo.clone_from(repository_url, os.path.join(working_dir, project_name))
+
+        cx = ConfigExtractor([os.path.join(working_dir, extractor_path)], create_venv=True)
+        assert set(extractors) == set(cx.parsers.keys())
