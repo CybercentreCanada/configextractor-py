e4157e015be2f872050e549ceaaa711fd8bbeb04
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2024-10-27 13:52:26 +0000

    Add special testing for extractors from CAPE

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -46,89 +46,8 @@ def test_mwcp_detection(cx):
             8,
             None,
         ),
-        (
-            "https://github.com/cccs-rs/CAPEv2",
-            "CAPEv2",
-            [
-                "CAPEv2.modules.processing.parsers.MACO.AgentTesla.AgentTesla",
-                "CAPEv2.modules.processing.parsers.MACO.AsyncRAT.AsyncRAT",
-                "CAPEv2.modules.processing.parsers.MACO.AuroraStealer.AuroraStealer",
-                "CAPEv2.modules.processing.parsers.MACO.Azorult.Azorult",
-                "CAPEv2.modules.processing.parsers.MACO.BackOffLoader.BackOffLoader",
-                "CAPEv2.modules.processing.parsers.MACO.BackOffPOS.BackOffPOS",
-                "CAPEv2.modules.processing.parsers.MACO.BitPaymer.BitPaymer",
-                "CAPEv2.modules.processing.parsers.MACO.BlackDropper.BlackDropper",
-                "CAPEv2.modules.processing.parsers.MACO.BlackNix.BlackNix",
-                "CAPEv2.modules.processing.parsers.MACO.Blister.Blister",
-                "CAPEv2.modules.processing.parsers.MACO.BruteRatel.BruteRatel",
-                "CAPEv2.modules.processing.parsers.MACO.BuerLoader.BuerLoader",
-                "CAPEv2.modules.processing.parsers.MACO.BumbleBee.BumbleBee",
-                "CAPEv2.modules.processing.parsers.MACO.Carbanak.Carbanak",
-                "CAPEv2.modules.processing.parsers.MACO.ChChes.ChChes",
-                "CAPEv2.modules.processing.parsers.MACO.CobaltStrikeBeacon.CobaltStrikeBeacon",
-                "CAPEv2.modules.processing.parsers.MACO.CobaltStrikeStager.CobaltStrikeStager",
-                "CAPEv2.modules.processing.parsers.MACO.DCRat.DCRat",
-                "CAPEv2.modules.processing.parsers.MACO.DarkGate.DarkGate",
-                "CAPEv2.modules.processing.parsers.MACO.DoppelPaymer.DoppelPaymer",
-                "CAPEv2.modules.processing.parsers.MACO.DridexLoader.DridexLoader",
-                "CAPEv2.modules.processing.parsers.MACO.Emotet.Emotet",
-                "CAPEv2.modules.processing.parsers.MACO.Enfal.Enfal",
-                "CAPEv2.modules.processing.parsers.MACO.EvilGrab.EvilGrab",
-                "CAPEv2.modules.processing.parsers.MACO.Fareit.Fareit",
-                "CAPEv2.modules.processing.parsers.MACO.Formbook.Formbook",
-                "CAPEv2.modules.processing.parsers.MACO.Greame.Greame",
-                "CAPEv2.modules.processing.parsers.MACO.GuLoader.GuLoader",
-                "CAPEv2.modules.processing.parsers.MACO.Hancitor.Hancitor",
-                "CAPEv2.modules.processing.parsers.MACO.HttpBrowser.HttpBrowser",
-                "CAPEv2.modules.processing.parsers.MACO.IcedID.IcedID",
-                "CAPEv2.modules.processing.parsers.MACO.IcedIDLoader.IcedIDLoader",
-                "CAPEv2.modules.processing.parsers.MACO.KoiLoader.KoiLoader",
-                "CAPEv2.modules.processing.parsers.MACO.Latrodectus.Latrodectus",
-                "CAPEv2.modules.processing.parsers.MACO.LokiBot.LokiBot",
-                "CAPEv2.modules.processing.parsers.MACO.Lumma.Lumma",
-                "CAPEv2.modules.processing.parsers.MACO.NanoCore.NanoCore",
-                "CAPEv2.modules.processing.parsers.MACO.Nighthawk.Nighthawk",
-                "CAPEv2.modules.processing.parsers.MACO.Njrat.Njrat",
-                "CAPEv2.modules.processing.parsers.MACO.Oyster.Oyster",
-                "CAPEv2.modules.processing.parsers.MACO.Pandora.Pandora",
-                "CAPEv2.modules.processing.parsers.MACO.PhemedroneStealer.PhemedroneStealer",
-                "CAPEv2.modules.processing.parsers.MACO.PikaBot.PikaBot",
-                "CAPEv2.modules.processing.parsers.MACO.PlugX.PlugX",
-                "CAPEv2.modules.processing.parsers.MACO.PoisonIvy.PoisonIvy",
-                "CAPEv2.modules.processing.parsers.MACO.Punisher.Punisher",
-                "CAPEv2.modules.processing.parsers.MACO.QakBot.QakBot",
-                "CAPEv2.modules.processing.parsers.MACO.QuasarRAT.QuasarRAT",
-                "CAPEv2.modules.processing.parsers.MACO.Quickbind.Quickbind",
-                "CAPEv2.modules.processing.parsers.MACO.RCSession.RCSession",
-                "CAPEv2.modules.processing.parsers.MACO.REvil.REvil",
-                "CAPEv2.modules.processing.parsers.MACO.RedLeaf.RedLeaf",
-                "CAPEv2.modules.processing.parsers.MACO.RedLine.RedLine",
-                "CAPEv2.modules.processing.parsers.MACO.Remcos.Remcos",
-                "CAPEv2.modules.processing.parsers.MACO.Retefe.Retefe",
-                "CAPEv2.modules.processing.parsers.MACO.Rhadamanthys.Rhadamanthys",
-                "CAPEv2.modules.processing.parsers.MACO.Rozena.Rozena",
-                "CAPEv2.modules.processing.parsers.MACO.SmallNet.SmallNet",
-                "CAPEv2.modules.processing.parsers.MACO.SmokeLoader.SmokeLoader",
-                "CAPEv2.modules.processing.parsers.MACO.Socks5Systemz.Socks5Systemz",
-                "CAPEv2.modules.processing.parsers.MACO.SparkRAT.SparkRAT",
-                "CAPEv2.modules.processing.parsers.MACO.SquirrelWaffle.SquirrelWaffle",
-                "CAPEv2.modules.processing.parsers.MACO.Stealc.Stealc",
-                "CAPEv2.modules.processing.parsers.MACO.Strrat.Strrat",
-                "CAPEv2.modules.processing.parsers.MACO.TSCookie.TSCookie",
-                "CAPEv2.modules.processing.parsers.MACO.TrickBot.TrickBot",
-                "CAPEv2.modules.processing.parsers.MACO.UrsnifV3.UrsnifV3",
-                "CAPEv2.modules.processing.parsers.MACO.VenomRat.VenomRAT",
-                "CAPEv2.modules.processing.parsers.MACO.WarzoneRAT.WarzoneRAT",
-                "CAPEv2.modules.processing.parsers.MACO.XWorm.XWorm",
-                "CAPEv2.modules.processing.parsers.MACO.XenoRAT.XenoRAT",
-                "CAPEv2.modules.processing.parsers.MACO.Zloader.Zloader",
-                "CAPEv2.modules.processing.parsers.mwcp.SmokeLoader.SmokeLoader",
-            ],
-            10,
-            "extractor/to_MACO",
-        ),
     ],
-    ids=("jeFF0Falltrades/rat_king_parser", "apophis133/apophis-YARA-Rules", "kevoreilly/CAPEv2"),
+    ids=("jeFF0Falltrades/rat_king_parser", "apophis133/apophis-YARA-Rules"),
 )
 def test_public_projects(repository_url: str, extractor_path: str, extractors: list, python_minor: int, branch: str):
     # Ensure that any changes we make doesn't break usage of public projects
@@ -146,5 +65,111 @@ def test_public_projects(repository_url: str, extractor_path: str, extractors: l
 
             cx = ConfigExtractor([os.path.join(working_dir, extractor_path)], create_venv=True)
             assert set(extractors) == set(cx.parsers.keys())
+    else:
+        pytest.skip("Unsupported Python version")
+
+
+CAPE_EXTRACTORS = [
+    "CAPEv2.modules.processing.parsers.MACO.AgentTesla.AgentTesla",
+    "CAPEv2.modules.processing.parsers.MACO.AsyncRAT.AsyncRAT",
+    "CAPEv2.modules.processing.parsers.MACO.AuroraStealer.AuroraStealer",
+    "CAPEv2.modules.processing.parsers.MACO.Azorult.Azorult",
+    "CAPEv2.modules.processing.parsers.MACO.BackOffLoader.BackOffLoader",
+    "CAPEv2.modules.processing.parsers.MACO.BackOffPOS.BackOffPOS",
+    "CAPEv2.modules.processing.parsers.MACO.BitPaymer.BitPaymer",
+    "CAPEv2.modules.processing.parsers.MACO.BlackDropper.BlackDropper",
+    "CAPEv2.modules.processing.parsers.MACO.BlackNix.BlackNix",
+    "CAPEv2.modules.processing.parsers.MACO.Blister.Blister",
+    "CAPEv2.modules.processing.parsers.MACO.BruteRatel.BruteRatel",
+    "CAPEv2.modules.processing.parsers.MACO.BuerLoader.BuerLoader",
+    "CAPEv2.modules.processing.parsers.MACO.BumbleBee.BumbleBee",
+    "CAPEv2.modules.processing.parsers.MACO.Carbanak.Carbanak",
+    "CAPEv2.modules.processing.parsers.MACO.ChChes.ChChes",
+    "CAPEv2.modules.processing.parsers.MACO.CobaltStrikeBeacon.CobaltStrikeBeacon",
+    "CAPEv2.modules.processing.parsers.MACO.CobaltStrikeStager.CobaltStrikeStager",
+    "CAPEv2.modules.processing.parsers.MACO.DCRat.DCRat",
+    "CAPEv2.modules.processing.parsers.MACO.DarkGate.DarkGate",
+    "CAPEv2.modules.processing.parsers.MACO.DoppelPaymer.DoppelPaymer",
+    "CAPEv2.modules.processing.parsers.MACO.DridexLoader.DridexLoader",
+    "CAPEv2.modules.processing.parsers.MACO.Emotet.Emotet",
+    "CAPEv2.modules.processing.parsers.MACO.Enfal.Enfal",
+    "CAPEv2.modules.processing.parsers.MACO.EvilGrab.EvilGrab",
+    "CAPEv2.modules.processing.parsers.MACO.Fareit.Fareit",
+    "CAPEv2.modules.processing.parsers.MACO.Formbook.Formbook",
+    "CAPEv2.modules.processing.parsers.MACO.Greame.Greame",
+    "CAPEv2.modules.processing.parsers.MACO.GuLoader.GuLoader",
+    "CAPEv2.modules.processing.parsers.MACO.Hancitor.Hancitor",
+    "CAPEv2.modules.processing.parsers.MACO.HttpBrowser.HttpBrowser",
+    "CAPEv2.modules.processing.parsers.MACO.IcedID.IcedID",
+    "CAPEv2.modules.processing.parsers.MACO.IcedIDLoader.IcedIDLoader",
+    "CAPEv2.modules.processing.parsers.MACO.KoiLoader.KoiLoader",
+    "CAPEv2.modules.processing.parsers.MACO.Latrodectus.Latrodectus",
+    "CAPEv2.modules.processing.parsers.MACO.LokiBot.LokiBot",
+    "CAPEv2.modules.processing.parsers.MACO.Lumma.Lumma",
+    "CAPEv2.modules.processing.parsers.MACO.NanoCore.NanoCore",
+    "CAPEv2.modules.processing.parsers.MACO.Nighthawk.Nighthawk",
+    "CAPEv2.modules.processing.parsers.MACO.Njrat.Njrat",
+    "CAPEv2.modules.processing.parsers.MACO.Oyster.Oyster",
+    "CAPEv2.modules.processing.parsers.MACO.Pandora.Pandora",
+    "CAPEv2.modules.processing.parsers.MACO.PhemedroneStealer.PhemedroneStealer",
+    "CAPEv2.modules.processing.parsers.MACO.PikaBot.PikaBot",
+    "CAPEv2.modules.processing.parsers.MACO.PlugX.PlugX",
+    "CAPEv2.modules.processing.parsers.MACO.PoisonIvy.PoisonIvy",
+    "CAPEv2.modules.processing.parsers.MACO.Punisher.Punisher",
+    "CAPEv2.modules.processing.parsers.MACO.QakBot.QakBot",
+    "CAPEv2.modules.processing.parsers.MACO.QuasarRAT.QuasarRAT",
+    "CAPEv2.modules.processing.parsers.MACO.Quickbind.Quickbind",
+    "CAPEv2.modules.processing.parsers.MACO.RCSession.RCSession",
+    "CAPEv2.modules.processing.parsers.MACO.REvil.REvil",
+    "CAPEv2.modules.processing.parsers.MACO.RedLeaf.RedLeaf",
+    "CAPEv2.modules.processing.parsers.MACO.RedLine.RedLine",
+    "CAPEv2.modules.processing.parsers.MACO.Remcos.Remcos",
+    "CAPEv2.modules.processing.parsers.MACO.Retefe.Retefe",
+    "CAPEv2.modules.processing.parsers.MACO.Rhadamanthys.Rhadamanthys",
+    "CAPEv2.modules.processing.parsers.MACO.Rozena.Rozena",
+    "CAPEv2.modules.processing.parsers.MACO.SmallNet.SmallNet",
+    "CAPEv2.modules.processing.parsers.MACO.SmokeLoader.SmokeLoader",
+    "CAPEv2.modules.processing.parsers.MACO.Socks5Systemz.Socks5Systemz",
+    "CAPEv2.modules.processing.parsers.MACO.SparkRAT.SparkRAT",
+    "CAPEv2.modules.processing.parsers.MACO.SquirrelWaffle.SquirrelWaffle",
+    "CAPEv2.modules.processing.parsers.MACO.Stealc.Stealc",
+    "CAPEv2.modules.processing.parsers.MACO.Strrat.Strrat",
+    "CAPEv2.modules.processing.parsers.MACO.TSCookie.TSCookie",
+    "CAPEv2.modules.processing.parsers.MACO.TrickBot.TrickBot",
+    "CAPEv2.modules.processing.parsers.MACO.UrsnifV3.UrsnifV3",
+    "CAPEv2.modules.processing.parsers.MACO.VenomRat.VenomRAT",
+    "CAPEv2.modules.processing.parsers.MACO.WarzoneRAT.WarzoneRAT",
+    "CAPEv2.modules.processing.parsers.MACO.XWorm.XWorm",
+    "CAPEv2.modules.processing.parsers.MACO.XenoRAT.XenoRAT",
+    "CAPEv2.modules.processing.parsers.MACO.Zloader.Zloader",
+    "CAPEv2.modules.processing.parsers.mwcp.SmokeLoader.SmokeLoader",
+]
+
+
+def test_CAPEv2():
+    # Ensure that any changes we make doesn't break usage of public projects
+    # which can affect downstream systems using like library (ie. Assemblyline)
+    import os
+    import sys
+    import shutil
+
+    from git import Repo
+    from tempfile import TemporaryDirectory
+
+    # TODO: Update this respective of https://github.com/kevoreilly/CAPEv2/pull/2373
+    main_repository = "https://github.com/cccs-rs/CAPEv2"
+    community_repository = "https://github.com/CAPESandbox/community"
+    if sys.version_info >= (3, 10):
+        with TemporaryDirectory() as working_dir:
+            main_folder = os.path.join(working_dir, "CAPEv2")
+            community_folder = os.path.join(working_dir, "community")
+
+            # Merge community extensions with main project
+            Repo.clone_from(main_repository, main_folder, depth=1, branch="extractor/to_MACO")
+            Repo.clone_from(community_repository, community_folder, depth=1)
+            shutil.copytree(community_folder, main_folder, dirs_exist_ok=True)
+
+            cx = ConfigExtractor([main_folder], create_venv=True)
+            assert set(CAPE_EXTRACTORS) == set(cx.parsers.keys())
     else:
         pytest.skip("Unsupported Python version")
